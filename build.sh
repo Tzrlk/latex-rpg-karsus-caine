464c6e49a9a7d11e34a2593382b3c77367d69afd
pcummuskey <pcummuskey@bravurasolutions.com>
2017-11-06 10:20:34 +1300

    Making sure all the files are built.

--- build.sh
+++ build.sh
@@ -4,8 +4,11 @@
 mkdir -p _build
 
 # Run latex on all tex files.
-pdflatex \
-	-include-directory lib \
-	-output-directory _build \
-	*.tex
+for file in *.tex; do
+	pdflatex \
+		-include-directory lib \
+		-output-directory _build \
+		-interaction errorstopmode \
+		${file}
+done
 
